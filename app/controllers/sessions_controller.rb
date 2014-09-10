f315b4e7f20f79f627efbab7ef5fc2ae08a3ac42
Geoffrey Clark <clark@justabeauty.com>
2014-09-10 14:41:50 -0400

    8.5 Exercise #1: form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+=begin
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+=end
+	
+	def create
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
 			sign_in user
 			redirect_to user
 		else
