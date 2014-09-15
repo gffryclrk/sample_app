b46aaa4c8299fc1c12608a21ec0f2fcb7c03da0f
Geoffrey Clark <clark@justabeauty.com>
2014-09-15 12:32:06 -0400

    9.2.1 Requiring Signed in Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  
 	def show
 		@user = User.find(params[:id])
 	end
@@ -25,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #handle successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -36,5 +39,11 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    #Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
   	
 end
