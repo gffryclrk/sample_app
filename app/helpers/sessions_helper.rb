f14ca7905da6d19feda152139db2ddc013528c75
Geoffrey Clark <clark@justabeauty.com>
2014-11-01 08:49:00 -0400

    10.3.2 Broken: Param not found

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-
+	
 	def sign_in(user)
 		remember_token = User.new_remember_token
 		cookies.permanent[:remember_token] = remember_token
@@ -21,15 +21,15 @@ module SessionsHelper
 	end
 
 	def current_user?(user)
-		user == current_user
-	end
+    user == current_user
+  end
 
-	def signed_in_user
-		unless signed_in?
-			store_location
-			redirect_to signin_url, notice: "Please sign in."
-		end
-	end
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 
 	def sign_out
 		current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
