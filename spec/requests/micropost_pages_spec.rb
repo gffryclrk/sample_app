a86b780e09dad821a03b48cb398593bd84b585a3
Geoffrey Clark <clark@justabeauty.com>
2014-11-03 15:34:40 -0500

    10.3.4 Destroying microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "MicropostPages" do
+describe "Micropost pages" do
   
   subject { page }
 
@@ -29,5 +29,17 @@ describe "MicropostPages" do
   			expect { click_button "Post" }.to change(Micropost, :count).by(1)
   		end
   	end
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+      end
+    end
   end
 end
